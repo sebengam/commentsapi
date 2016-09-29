c6ec4cf32e9c09c65e4bac1103408237b4ac28db
Rosie <211020796@mycput.ac.za>
2016-09-29 18:31:09 +0200

    Some more libraries under .idea User package factory and tests done

--- app/domain/users/Reputation.scala
+++ app/domain/users/Reputation.scala
@@ -1,6 +1,7 @@
 package domain.users
 
 import org.joda.time.DateTime
+import play.api.libs.json.Json
 
 /**
   * Created by hashcode on 2016/09/21.
@@ -8,5 +9,5 @@ import org.joda.time.DateTime
 case class Reputation(emailId:String, date:DateTime,value:Int)
 
 object Reputation{
-
+  //implicit val abuseFmt = Json.format[Reputation]
 }
