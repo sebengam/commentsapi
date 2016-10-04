404c2fe7ce152d345c294c6ff4223b777cbeec5d
sebengam <sebengam@cput.ac.za>
2016-10-04 18:22:17 +0200

    test

--- app/factories/votes/VoteFactory.scala
+++ app/factories/votes/VoteFactory.scala
@@ -1,12 +1,15 @@
 package factories.votes
 
+import domain.votes.VoteUp
+
 /**
   * Created by hashcode on 2016/09/21.
   */
 object VoteFactory {
-  def createVoteUp(values:Map[String,String],count:Int)={
+  def getVotes(values:Map[String,String],count:Int){}//:VoteUp = {
 
-  }
+    //VoteUp(commentIdOrResponseId = values("commentIdOrResponseId"),emailId= values("emailId"),ipaddress = values("ipaddress"),count = values(0))
+  //}
 
   def createVoteDown(values:Map[String,String],count:Int)={
 
@@ -15,5 +18,9 @@ object VoteFactory {
   def createVoterRegister(values:Map[String,String])={
 
   }
+//case class VoteDown(commentIdOrResponseId:String,emailId:String,ipaddress:String,count:Int);
+ //case class VoteDown(commentIdOrResponseId:String,emailId:String,ipaddress:String,count:Int);
+ // VoteUp(commentIdOrResponseId:String,emailId:String,ipaddress:String,count:Int);
+
 
 }
