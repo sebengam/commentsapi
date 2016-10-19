67eb78248ded2aae045aee51d27aba0958edb9c5
Boniface Kabaso <boniface@kabaso.com>
2016-10-19 09:36:46 +0200

    Updated Repo to Remove Wrong Committed Files

--- app/domain/comments/Abuse.scala
+++ app/domain/comments/Abuse.scala
@@ -5,7 +5,11 @@ import org.joda.time.DateTime
 /**
   * Created by hashcode on 2016/09/21.
   */
-case class Abuse(siteId:String,subjectId:String, commentIdOrResponseId:String, abuseId:String, details:String, emailId:String, date:DateTime)
+case class Abuse(siteId:String,
+                 subjectId:String,
+                 commentIdOrResponseId:String,
+                 abuseId:String, details:String,
+                 emailId:String, date:DateTime)
 object Abuse{
 
 }
--- app/domain/comments/Comment.scala
+++ app/domain/comments/Comment.scala
@@ -5,8 +5,14 @@ import org.joda.time.DateTime
 /**
   * Created by hashcode on 2016/09/20.
   */
-case class Comment(siteId:String,subjectId:String,commentId:String,comment:String, emailId:String, ipaddress:String,date:DateTime)
+case class Comment(siteId: String,
+                   subjectId: String,
+                   commentId: String,
+                   comment: String,
+                   emailId: String,
+                   ipaddress: String,
+                   date: DateTime)
 
-object Comment{
+object Comment {
 
 }
